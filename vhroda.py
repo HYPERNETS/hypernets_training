643f6a43824c946f56b4abd4b290ad657a5e652e
Pieter De Vis <pieter.de.vis@npl.co.uk>
2024-12-02 13:30:45 +0000

    LANDHYPERNET improve notebook

--- vhroda.py
+++ vhroda.py
@@ -8,142 +8,34 @@ from obsarray.templater.dataset_util import DatasetUtil
 import matplotlib.pyplot as plt
 import os
 
-dates =
+dates = ['20241025', '20241017', '20241009', '20241001', '20220606']
 
-ds_refl_L9 = xr.load_dataset("example_L9_20220606.nc")
-ds_refl_L9_rcn = xr.load_dataset("example_L9_20220606_rcn.nc")
-
-bands_L9 = ["B1", "B2", "B3", "B4", "B5", "B6"]
-wav_L9 = [
-    442.98244284,
-    482.58889933,
-    561.33224557,
-    654.60554515,
-    864.5708545,
-    1609.09056245,
-]
-
-refl_L9 = np.array([np.mean(ds_refl_L9[band].values) for band in bands_L9])
-u_refl_L9 = np.array([np.std(ds_refl_L9[band].values) for band in bands_L9])
-refl_L9_rcn = np.array([np.mean(ds_refl_L9_rcn[band].values) for band in bands_L9])
-u_refl_L9_rcn = np.array([np.std(ds_refl_L9_rcn[band].values) for band in bands_L9])
-
-
-def band_integrate_L9(reflectance, wavelength):
-    """
-    Function to perform L9 band integration on reflectance
-
-    :param reflectance: reflectance spectrum
-    :param wavelength: wavelengths
-    """
-    refl_band, band_centres = band_integration.spectral_band_int_sensor(
-        d=reflectance,
-        wl=wavelength,
-        platform_name="Landsat-8",
-        sensor_name="OLI",
-        u_d=None,
-    )
-    return refl_band[:6]
-
-ds_HYP = xr.open_dataset(
-    "HYPERNETS_L_GHNA_L2A_REF_20220606T0900_20231226T1435_v2.0.nc"
-)  # read digital effects table
-
-bad_flags = [
-    "pt_ref_invalid",
-    "half_of_scans_masked",
-    "not_enough_dark_scans",
-    "not_enough_rad_scans",
-    "not_enough_irr_scans",
-    "no_clear_sky_irradiance",
-    "variable_irradiance",
-    "half_of_uncertainties_too_big",
-    "discontinuity_VNIR_SWIR",
-    "single_irradiance_used",
-]
-flagged = DatasetUtil.get_flags_mask_or(ds_HYP["quality_flag"], bad_flags)
-id_series_valid = np.where(~flagged)[0]
-ds_HYP = ds_HYP.isel(series=id_series_valid)
-
-vza = 0.7
-vaa = 38.1
-vzadiff = ds_HYP["viewing_zenith_angle"].values - vza
-vaadiff = np.abs(ds_HYP["viewing_azimuth_angle"].values - vaa % 360)
-angledif_series = vzadiff**2 + vaadiff**2
-id_series = np.where(angledif_series**2 == np.min(angledif_series**2))[0][0]
-ds_HYP = ds_HYP.isel(series=id_series)
-
-wav_HYP_full = ds_HYP["wavelength"].values
-refl_HYP_full = ds_HYP["reflectance"].values
-u_ran_refl_HYP_full = refl_HYP_full * ds_HYP["u_rel_random_reflectance"].values / 100
-u_sys_refl_HYP_full = refl_HYP_full * ds_HYP["u_rel_systematic_reflectance"].values / 100
-u_refl_HYP_full = np.sqrt(u_ran_refl_HYP_full**2+u_sys_refl_HYP_full**2)
-
-#
-
-refl_HYP = band_integrate_L9(refl_HYP_full,wav_HYP_full)
-
-prop = MCPropagation(50, parallel_cores=1)
-u_ran_refl_HYP = prop.propagate_standard(band_integrate_L9,[refl_HYP_full,wav_HYP_full],[u_ran_refl_HYP_full,None],["rand",None])
-u_sys_refl_HYP = prop.propagate_standard(band_integrate_L9,[refl_HYP_full,wav_HYP_full],[u_sys_refl_HYP_full,None],[ds_HYP["err_corr_systematic_reflectance"].values,None])
-u_refl_HYP = np.sqrt(u_ran_refl_HYP**2+u_sys_refl_HYP**2)
-
-rcn_data = np.genfromtxt(
-    "GONA01_2022_157_v00.09.input", dtype="str", delimiter="\t", skip_header=4
-)
-if np.all(rcn_data[:, -1] == ""):
-    rcn_data = rcn_data[:, :-1]
-wav_rcn = rcn_data[12:223, 0].astype(float)
-times_rcn = rcn_data[2, 1:]
-
-refl_rcn = rcn_data[12:223, 3].astype(float)
-u_refl_rcn = np.abs(rcn_data[229:440, 3].astype(float))
-
-(
-    refl_rcn_band,
-    band_centres,
-    u_refl_rcn_band,
-) = band_integration.spectral_band_int_sensor(
-    d=refl_rcn,
-    wl=wav_rcn,
-    platform_name="Landsat-8",
-    sensor_name="OLI",
-    u_d=u_refl_rcn,
-)
-refl_rcn_band, u_refl_rcn_band = refl_rcn_band[:6], u_refl_rcn_band[:6]
-
-bias = ((refl_L9 / refl_HYP) - 1) * 100
-u_bias = np.sqrt((u_refl_L9 / refl_L9) ** 2 + (u_refl_HYP / refl_HYP) ** 2) * 100
-
-bias_rcn = ((refl_L9_rcn / refl_rcn_band) - 1) * 100
-u_bias_rcn = (
-    np.sqrt((u_refl_L9_rcn / refl_L9_rcn) ** 2 + (u_refl_rcn_band / refl_rcn_band) ** 2)
-    * 100
-)
+hypernet_paths = ['HYPERNETS_L_GHNA_L2A_REF_20241025T0900_20241114T0243_v2.1.nc', 'HYPERNETS_L_GHNA_L2A_REF_20241017T0900_20241114T0343_v2.1.nc', 'HYPERNETS_L_GHNA_L2A_REF_20241009T0901_20241016T1731_v2.1.nc', 'HYPERNETS_L_GHNA_L2A_REF_20241001T0900_20241001T1525_v2.1.nc', 'HYPERNETS_L_GHNA_L2A_REF_20220606T0900_20231226T1435_v2.0.nc']
+rcn_paths = ['GONA01_2024_299_v00.09.input','GONA01_2024_291_v00.09.input','GONA01_2024_283_v00.09.input','GONA01_2024_275_v00.09.input','GONA01_2022_157_v00.09.input']
 
 
 def plot(
-    path: str,
-    sat: str,
-    name: str,
-    date: str,
-    sat_wav: np.ndarray,
-    sat_refl: np.ndarray,
-    sat_unc: np.ndarray,
-    hyp_wav: np.ndarray,
-    hyp_refl: np.ndarray,
-    hyp_unc: np.ndarray,
-    rcn_wav: np.ndarray,
-    rcn_refl: np.ndarray,
-    rcn_unc: np.ndarray,
-    bias: np.ndarray,
-    bias_unc: np.ndarray,
-    wavs_band: np.ndarray,
-    reflectance_band: np.ndarray,
-    reflectance_band_unc: np.ndarray,
-    vza: float,
-    bias_rcn: np.ndarray,
-    bias_rcn_unc: np.ndarray,
+        path: str,
+        sat: str,
+        name: str,
+        date: str,
+        sat_wav: np.ndarray,
+        sat_refl: np.ndarray,
+        sat_unc: np.ndarray,
+        hyp_wav: np.ndarray,
+        hyp_refl: np.ndarray,
+        hyp_unc: np.ndarray,
+        rcn_wav: np.ndarray,
+        rcn_refl: np.ndarray,
+        rcn_unc: np.ndarray,
+        bias: np.ndarray,
+        bias_unc: np.ndarray,
+        wavs_band: np.ndarray,
+        reflectance_band: np.ndarray,
+        reflectance_band_unc: np.ndarray,
+        vza: float,
+        bias_rcn: np.ndarray,
+        bias_rcn_unc: np.ndarray,
 ):
     """
     Function to plot matchup results
@@ -247,29 +139,146 @@ def plot(
     plt.close()
 
 
-plot(
-    "./",
-    "LANDSAT 9",
-    "bias_L9_test",
-    "2022-06-06",
-    wav_L9,
-    refl_L9,
-    u_refl_L9,
-    wav_HYP_full,
-    refl_HYP_full,
-    u_refl_HYP_full,
-    wav_rcn,
-    refl_rcn,
-    u_refl_rcn,
-    bias,
-    u_bias,
-    wav_L9,
-    refl_HYP,
-    u_refl_HYP,
-    vza,
-    bias_rcn,
-    u_bias_rcn,
-)
+
+for i,date in enumerate(dates):
+
+    ds_refl_L9 = xr.load_dataset("example_L9_%s.nc"%date)
+    ds_refl_L9_rcn = xr.load_dataset("example_L9_%s_rcn.nc"%date)
+
+    bands_L9 = ["B1", "B2", "B3", "B4", "B5", "B6"]
+    wav_L9 = [
+        442.98244284,
+        482.58889933,
+        561.33224557,
+        654.60554515,
+        864.5708545,
+        1609.09056245,
+    ]
+
+    refl_L9 = np.array([np.mean(ds_refl_L9[band].values) for band in bands_L9])
+    u_refl_L9 = np.array([np.std(ds_refl_L9[band].values) for band in bands_L9])
+    refl_L9_rcn = np.array([np.mean(ds_refl_L9_rcn[band].values) for band in bands_L9])
+    u_refl_L9_rcn = np.array([np.std(ds_refl_L9_rcn[band].values) for band in bands_L9])
+
+
+    def band_integrate_L9(reflectance, wavelength):
+        """
+        Function to perform L9 band integration on reflectance
+
+        :param reflectance: reflectance spectrum
+        :param wavelength: wavelengths
+        """
+        refl_band, band_centres = band_integration.spectral_band_int_sensor(
+            d=reflectance,
+            wl=wavelength,
+            platform_name="Landsat-8",
+            sensor_name="OLI",
+            u_d=None,
+        )
+        return refl_band[:6]
+
+    ds_HYP = xr.open_dataset(
+        hypernet_paths[i]
+    )  # read digital effects table
+
+    bad_flags = [
+        "pt_ref_invalid",
+        "half_of_scans_masked",
+        "not_enough_dark_scans",
+        "not_enough_rad_scans",
+        "not_enough_irr_scans",
+        "no_clear_sky_irradiance",
+        "variable_irradiance",
+        "half_of_uncertainties_too_big",
+        "discontinuity_VNIR_SWIR",
+        "single_irradiance_used",
+    ]
+    flagged = DatasetUtil.get_flags_mask_or(ds_HYP["quality_flag"], bad_flags)
+    id_series_valid = np.where(~flagged)[0]
+    ds_HYP = ds_HYP.isel(series=id_series_valid)
+
+    vza = 10
+    #vaa = 83
+    vzadiff = ds_HYP["viewing_zenith_angle"].values - vza
+    #vaadiff = np.abs(ds_HYP["viewing_azimuth_angle"].values - vaa % 360)
+    angledif_series = np.sqrt(vzadiff**2)
+    id_series = np.where(angledif_series < 5)[0]
+    ds_HYP = ds_HYP.isel(series=id_series)
+
+    wav_HYP_full = ds_HYP["wavelength"].values
+    refl_HYP_full = np.mean(ds_HYP["reflectance"].values,axis=1)
+    u_ran_refl_HYP_full = refl_HYP_full * np.mean(ds_HYP["u_rel_random_reflectance"].values,axis=1) / 100
+    u_sys_refl_HYP_full = refl_HYP_full * np.mean(ds_HYP["u_rel_systematic_reflectance"].values,axis=1) / 100
+    u_refl_HYP_full = np.sqrt(u_ran_refl_HYP_full**2+u_sys_refl_HYP_full**2)
+
+    #
+
+    refl_HYP = band_integrate_L9(refl_HYP_full,wav_HYP_full)
+
+    prop = MCPropagation(50, parallel_cores=1)
+    u_ran_refl_HYP = prop.propagate_standard(band_integrate_L9,[refl_HYP_full,wav_HYP_full],[u_ran_refl_HYP_full,None],["rand",None])
+    u_sys_refl_HYP = prop.propagate_standard(band_integrate_L9,[refl_HYP_full,wav_HYP_full],[u_sys_refl_HYP_full,None],[ds_HYP["err_corr_systematic_reflectance"].values,None])
+    u_refl_HYP = np.sqrt(u_ran_refl_HYP**2+u_sys_refl_HYP**2)
+
+    rcn_data = np.genfromtxt(
+        rcn_paths[i]  , dtype="str", delimiter="\t", skip_header=4
+    )
+    if np.all(rcn_data[:, -1] == ""):
+        rcn_data = rcn_data[:, :-1]
+    wav_rcn = rcn_data[12:223, 0].astype(float)
+    times_rcn = rcn_data[2, 1:]
+
+    refl_rcn = rcn_data[12:223, 3].astype(float)
+    u_refl_rcn = np.abs(rcn_data[229:440, 3].astype(float))
+
+    (
+        refl_rcn_band,
+        band_centres,
+        u_refl_rcn_band,
+    ) = band_integration.spectral_band_int_sensor(
+        d=refl_rcn,
+        wl=wav_rcn,
+        platform_name="Landsat-8",
+        sensor_name="OLI",
+        u_d=u_refl_rcn,
+    )
+    refl_rcn_band, u_refl_rcn_band = refl_rcn_band[:6], u_refl_rcn_band[:6]
+
+    bias = ((refl_L9 / refl_HYP) - 1) * 100
+    u_bias = np.sqrt((u_refl_L9 / refl_L9) ** 2 + (u_refl_HYP / refl_HYP) ** 2) * 100
+
+    bias_rcn = ((refl_L9_rcn / refl_rcn_band) - 1) * 100
+    u_bias_rcn = (
+        np.sqrt((u_refl_L9_rcn / refl_L9_rcn) ** 2 + (u_refl_rcn_band / refl_rcn_band) ** 2)
+        * 100
+    )
+
+
+
+
+    plot(
+        "./",
+        "LANDSAT 9",
+        "bias_L9_%s"%date,
+        date,
+        wav_L9,
+        refl_L9,
+        u_refl_L9,
+        wav_HYP_full,
+        refl_HYP_full,
+        u_refl_HYP_full,
+        wav_rcn,
+        refl_rcn,
+        u_refl_rcn,
+        bias,
+        u_bias,
+        wav_L9,
+        refl_HYP,
+        u_refl_HYP,
+        vza,
+        bias_rcn,
+        u_bias_rcn,
+    )
 
 
 ds_refl_S2 = xr.load_dataset("example_S2_20220628.nc")
@@ -383,4 +392,3 @@ plot(
 )
 
 
-for
